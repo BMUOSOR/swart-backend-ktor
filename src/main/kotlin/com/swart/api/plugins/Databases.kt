083e7bf8b1aaabf5bd7c1ad97f2b714b7fee1525
BMUOSOR <bmuosor@epsa.upv.es>
2026-05-03 20:22:04 +0200

    Add debug to print tables in database to find the ghost tables

--- src/main/kotlin/com/swart/api/plugins/Databases.kt
+++ src/main/kotlin/com/swart/api/plugins/Databases.kt
@@ -28,6 +28,18 @@ fun Application.configureDatabases() {
     Database.connect(dataSource)
     
     transaction {
+        println("=== INICIO DE DEBUG DE TABLAS EN SUPABASE ===")
+        try {
+            val stmt = connection.prepareStatement("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';", false)
+            val rs = stmt.executeQuery()
+            while (rs.next()) {
+                println("TABLA ENCONTRADA EN PUBLIC: '${rs.getString(1)}'")
+            }
+        } catch (e: Exception) {
+            println("ERROR LEYENDO TABLAS: ${e.message}")
+        }
+        println("=== FIN DE DEBUG DE TABLAS ===")
+
         SchemaUtils.create(
             Usuarios,
             Artistas,
